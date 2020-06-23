594f44de7b73152646da3997d39a25bb2dc4bb85
Sociosarbis <136657577@qq.com>
2020-06-23 23:24:16 +0800

    feat: 在隐藏列表项时触发钩子用于更新隐藏项的高度

--- src/js/dom/slideWindow.js
+++ src/js/dom/slideWindow.js
@@ -1,3 +1,5 @@
+import { noop } from '../const/common'
+
 class SlideItem {
   constructor(height) {
     this.height = height
@@ -18,7 +20,7 @@ class SlideItem {
   }
 }
 class SlideWindow {
-  constructor({ container, defaultItemSize, start = -1, end = -1, startY }) {
+  constructor({ container, defaultItemSize, start = -1, end = -1, startY, onHideItem = noop }) {
     this.container = container
     this.start = start
     this.end = end
@@ -29,7 +31,12 @@ class SlideWindow {
     this.offsetTopForEnd = 0
     this.upperFactor = 2
     this.lowerFactor = 1
+    /**
+     * @type {(item: SlideItem, idx: number) => any}
+     */
+    this.onHideItem = onHideItem
     this.items = []
+    this.defaultItem = Object.freeze(this.createDefaultItem())
   }
 
   resizeItemsNum(num) {
@@ -48,67 +55,49 @@ class SlideWindow {
     let threshold = this.container.scrollTop - this.lowerFactor * this.container.offsetHeight
     let start = this.start
     let acc = 0
-    let offsetTopForEnd = this.paddingTop
-    for (let i = start; i < end; i++) {
-      offsetTopForEnd += this.items[i].height
+    let endBottom = this.paddingTop
+    for (let i = start; i <= end; i++) {
+      endBottom += this.getItemOrDefault(i).height
     }
     // 当start的下边小于容器顶减两个容器高，则增加start至start的下边大于容器顶减一个容器高
     if (
-      paddingTop + this.items[start].height <
+      paddingTop + this.getItemOrDefault(start).height <
       this.container.scrollTop - this.upperFactor * this.container.offsetHeight
     ) {
-      acc = this.items[start].cacheHeight().height
       while (start < end) {
-        if (paddingTop + acc + this.items[start + 1].height < threshold) {
-          acc += this.items[++start].cacheHeight().height
-        } else {
-          start++
-          break
-        }
+        this.onHideItem(this.getItem(start), start)
+        acc += this.items[start++].cacheHeight().height
+        if (paddingTop + acc >= threshold) break
       }
       paddingTop += acc
       // 当start的上边大于容器顶减一个容器高，则减小start至start的上边小于容器顶减一个容器高
     } else if (paddingTop > threshold) {
-      acc = -(this.items[start].heightCache ? this.items[start].getAndRemoveHeightCache() : this.items[start].height)
-      this.items[start].remove
       while (start > 0) {
-        if (paddingTop + acc - this.items[start - 1].height >= threshold) {
-          --start
-          acc -= -(this.items[start].heightCache
-            ? this.items[start].getAndRemoveHeightCache()
-            : this.items[start].height)
-        } else {
-          start--
-          break
-        }
+        acc -= this.getItem(--start).heightCache
+          ? this.items[start].getAndRemoveHeightCache()
+          : this.items[start].height
+        if (paddingTop + acc <= threshold) break
       }
       paddingTop += acc
     }
     acc = 0
     threshold = containerBottom + this.lowerFactor * this.container.offsetHeight
     // 当end的下边小于容器底加一个容器高，则增加end至end的下边大于容器底加一个容器高
-    if (offsetTopForEnd + this.items[end].height < threshold) {
-      acc = this.items[end].heightCache ? this.items[end].getAndRemoveHeightCache() : this.items[end].height
+    if (endBottom < threshold) {
       while (end < this.items.length - 1) {
-        if (offsetTopForEnd + acc + this.items[end + 1].height <= threshold) {
-          ++end
-          acc += this.items[end].heightCache ? this.items[end].getAndRemoveHeightCache() : this.items[end].height
-        } else {
-          end++
-          break
-        }
+        acc += this.getItem(++end).heightCache ? this.items[end].getAndRemoveHeightCache() : this.items[end].height
+        if (endBottom + acc >= threshold) break
       }
       paddingBottom -= acc
       // 当end的上边大于容器底加两个容器高，则减小end至end的上边小于容器底加一个容器高
-    } else if (offsetTopForEnd > containerBottom + this.upperFactor * this.container.offsetHeight) {
-      acc = -this.items[end].cacheHeight().height
+    } else if (
+      endBottom - this.getItemOrDefault(end).height >
+      containerBottom + this.upperFactor * this.container.offsetHeight
+    ) {
       while (end > 0) {
-        if (offsetTopForEnd + acc - this.items[end - 1].height > threshold) {
-          acc -= this.items[--end].cacheHeight().height
-        } else {
-          end--
-          break
-        }
+        this.onHideItem(this.getItem(end), end)
+        acc -= this.items[end--].cacheHeight().height
+        if (endBottom + acc <= threshold) break
       }
       paddingBottom -= acc
     }
@@ -130,19 +119,36 @@ class SlideWindow {
 
   updateItem(idx, newProps) {
     const prevHeight = this.items[idx] ? this.items[idx].height : 0
-    if (!this.items[idx]) {
-      this.items[idx] = this.createDefaultItem()
-    }
+    const targetItem = this.getItem(idx)
     if (newProps) {
-      this.items[idx].height = newProps.height
+      targetItem.height = newProps.height
     }
-    const heightDelta = this.items[idx].height - prevHeight
-    if (idx < this.start) {
-      this.paddingTop += heightDelta
+    const heightDelta = targetItem.height - prevHeight
+    if (!targetItem.heightCache) {
+      if (idx < this.start) {
+        this.paddingTop += heightDelta
+      }
+      if (idx > this.end) {
+        this.paddingBottom += heightDelta
+      }
     }
-    if (idx > this.end) {
-      this.paddingBottom += heightDelta
+  }
+
+  getItemOrDefault(idx) {
+    if (idx < this.items.length) {
+      return this.items[idx] || this.defaultItem
     }
+    return null
+  }
+
+  getItem(idx) {
+    if (idx < this.items.length) {
+      if (!this.items[idx]) {
+        this.items[idx] = this.createDefaultItem()
+      }
+      return this.items[idx]
+    }
+    return false
   }
 }
 
