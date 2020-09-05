1c5a35cdb4ccc9947b28be2eae2f8c1c30598282
Sociosarbis <136657577@qq.com>
2020-09-05 20:29:40 +0800

    feat: EventEmitter provide the same functionality as $on $off in vue

--- src/js/task/EventEmitter.js
+++ src/js/task/EventEmitter.js
@@ -1,9 +1,21 @@
+import { isUndef } from '../obj/is'
+
 export default class EventEmitter {
   constructor() {
+    /** @type {Map<string, Set>} */
     this._bus = new Map()
   }
 
+  /**
+   * @param {string|string[]} eventName
+   */
   on(eventName, handler) {
+    Array.isArray(eventName) ? eventName.forEach((name) => this._on(name, handler)) : this._on(eventName, handler)
+  }
+  /**
+   * @param {string} eventName
+   */
+  _on(eventName, handler) {
     if (!this._bus.has(eventName)) this._bus.set(eventName, new Set())
     this._bus.get(eventName).add(handler)
   }
@@ -19,9 +31,20 @@ export default class EventEmitter {
     this.on(eventName, composed)
   }
 
+  /**
+   * @param {string|string[]} eventName
+   */
   off(eventName, handler) {
-    if (this._bus.has(eventName)) {
-      this._bus.get(eventName).delete(handler)
+    Array.isArray(eventName) ? eventName.forEach((name) => this._off(name, handler)) : this._off(eventName, handler)
+  }
+
+  _off(eventName, handler) {
+    if (isUndef(eventName)) this._bus.clear()
+    else {
+      if (this._bus.has(eventName)) {
+        if (isUndef(handler)) this._bus.get(eventName).clear()
+        else this._bus.get(eventName).delete(handler)
+      }
     }
   }
 
