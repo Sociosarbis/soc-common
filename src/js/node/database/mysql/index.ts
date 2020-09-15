0a20f45cc4018aa102ed6732c753f11da5e0fb03
Sociosarbis <136657577@qq.com>
2020-09-16 00:44:10 +0800

    feat: insert and update value

--- src/js/node/database/mysql/index.ts
+++ src/js/node/database/mysql/index.ts
@@ -3,8 +3,8 @@ import mysql from 'mysql'
 const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: '***************',
-  database: '****'
+  password: '',
+  database: ''
 })
 
 connection.connect()
@@ -18,9 +18,42 @@ connection.query('CREATE TABLE IF NOT EXISTS user_key_store (\n' +
   ' PRIMARY KEY(uid, `key`),\n' +
   ' INDEX uid_asc (uid)\n' +
   ') ENGINE=INNODB'
-  , (err, results, fields) => {
+  , (err, results) => {
   if (err) throw err
   console.log(results)
+})
+
+connection.query('DROP TRIGGER IF EXISTS user_key_store_update', (err, results) => {
+    if (err) throw err
+    console.log(results)
+  }
+)
+
+connection.query('CREATE TRIGGER user_key_store_update BEFORE UPDATE ON user_key_store FOR EACH ROW\n' +
+  'SET NEW.mtime = CURRENT_TIMESTAMP()', (err, results) => {
+    if (err) throw err
+    console.log(results)
+  }
+)
+
+connection.query('INSERT IGNORE INTO user_key_store (uid, `key`, `value`)\n' +
+  "VALUES (20200916, 'birth', '19940321'),\n" +
+  "(20200916, 'pet', 'dog')", (err, results) => {
+    if (err) throw err
+    console.log(results)
+  })
+
+connection.query('UPDATE user_key_store\n' +
+  "SET `value` = 'cat'\n" +
+  "WHERE uid = 20200916 AND\n" +
+  "`key` = 'pet'", (err, results) => {
+    if (err) throw err
+    console.log(results)
+})
+
+connection.query("SELECT `value`, mtime FROM user_key_store WHERE `key` = 'pet' AND uid = 20200916 LIMIT 1", (err, results) => {
+    if (err) throw err
+    console.log(results)
 })
 
 connection.end()
