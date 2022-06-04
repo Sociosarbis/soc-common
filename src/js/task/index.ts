64087cd5743e490814cb48a59186e5603e99981a
sociosarbis <136657577@qq.com>
2022-06-04 23:35:17 +0800

    feat: :sparkles: raceAll

--- src/js/task/index.ts
+++ src/js/task/index.ts
@@ -23,7 +23,7 @@ type MeasureData = { time: number }
 async function measure(fn: (...args: any[]) => any, times = 1) {
   const data: MeasureData[] & Partial<{ avg: MeasureData }> = []
   for (let i = 0; i < times; i++) {
-    let before = performance.now()
+    const before = performance.now()
     await fn()
     data.push({ time: performance.now() - before })
   }
@@ -31,6 +31,36 @@ async function measure(fn: (...args: any[]) => any, times = 1) {
     time: data.reduce((acc, item) => acc + item.time, 0) / data.length
   }
   console.table(data)
+}
+
+export async function raceAll<T>(promiseFactories: Array<() => Promise<T>>, batchSize = 5) {
+  const ret: Array<T | undefined> = []
+  const remains = promiseFactories.map((fn, i) => [fn, i] as const)
+  const mapFn = ([fn, i]: readonly [typeof promiseFactories[0], number]) => {
+    const p = fn()
+      .then((res) => {
+        ret[i] = res
+      })
+      .catch(() => {
+        //
+      })
+      .finally(() => {
+        const index = tasks.indexOf(p)
+        if (index !== -1) {
+          tasks.splice(index, 1)
+        }
+      })
+    return p
+  }
+  const tasks = remains.splice(0, batchSize).map(mapFn)
+
+  while (tasks.length) {
+    await Promise.race(tasks)
+    if (remains.length) {
+      tasks.push(mapFn(remains.shift()))
+    }
+  }
+  return ret
 }
 
 export { debounce, sleep, measure }
